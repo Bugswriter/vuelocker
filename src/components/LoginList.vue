c5419cd691ca4a9356e9c060b9f23bae89c9ee3c
bugswriter <surajkushwah6969@gmail.com>
2020-06-13 02:30:41 +0530

    adding details of login

--- src/components/LoginList.vue
+++ src/components/LoginList.vue
@@ -1,50 +1,77 @@
 <template>
-  <div id="list-login">
-    <h2 class="m-3">Your Logins</h2>
-    <hr>
-    <b-col cols="10">
-      <b-card no-body class="mb-4">
-        <b-card-header header-tag="header" class="p-0" role="tab">
-          <b-button block v-b-toggle.accordion-1 variant="light" class="text-primary">chess.com</b-button>
-        </b-card-header>
-        <b-collapse id="accordion-1 visible" accordion="my-accordion" role="tabpanel">
-          <b-card-body>
-            <b-card-text></b-card-text>
-            <b-card-text>This is first login</b-card-text>
-          </b-card-body>
-        </b-collapse>
-      </b-card>
-
-      <b-card no-body class="mb-4">
-        <b-card-header header-tag="header" class="p-0 text-left" role="tab">
-          <b-button block v-b-toggle.accordion-2 variant="light" class="text-primary">twitter.com</b-button>
-        </b-card-header>
-        <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
-          <b-card-body>
-            <b-card-text>This is second login</b-card-text>
-          </b-card-body>
-        </b-collapse>
-      </b-card>
-
-      <b-card no-body class="mb-4">
-        <b-card-header header-tag="header" class="p-0" role="tab">
-          <b-button block v-b-toggle.accordion-3 variant="light" class="text-primary">facebook.com</b-button>
-        </b-card-header>
-        <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
-          <b-card-body>
-            <b-card-text>This is third Login</b-card-text>
-          </b-card-body>
+<div id="list-login">
+  <h2 class="m-3">Your Logins</h2>
+  <hr>
+  <b-col>    
+    <b-card v-for="login in logins" :key="login.id" no-body class="mb-4">
+      <b-card :header="login.website" v-b-toggle="'accordion-'+login.id" no-body class="p-0" role="tab">
+      <b-collapse :id="'accordion-'+login.id" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text class="float-right">
+            <b-button-group>
+              <b-button variant="light">🖉 edit</b-button>
+              <b-button variant="light">🗑️ remove</b-button>
+            </b-button-group>
+          </b-card-text>
+          <b-card-text>
+            <b-col cols="8">
+              <b-form>
+                <b-form-group id="input-group-2" label="👤 Username" label-for="input-2">
+                  <b-form-group inline>
+                    <b-form-input id="input-2" :value="login.username" size="lg" type="text" plaintext></b-form-input>
+                  </b-form-group>
+                </b-form-group>
+                <b-form-group id="input-group-3" label="🔑 Password" label-for="input-3" inline>
+                  <b-form-input class="mb-2 mr-sm-2 mb-sm-0" id="input-3" :value="login.password" size="lg" type="password" plaintext></b-form-input>
+                </b-form-group>
+              </b-form>
+            </b-col>
+          </b-card-text>
+        </b-card-body>
       </b-collapse>
+      </b-card>            
     </b-card>
-    </b-col>
-    
+
+  </b-col>
+
 </div>
 </template>
 
 <script type="text/javascript">
-  export default {
+export default {
+    data() {
+        return {
+            logins : [
+                {
+                    "id": 1,
+                    "website": "chess.com",
+                    "username": "bugswriter@protonmail.com",
+                    "password": "HorseRider212"
+                },
+                {
+                    "id": 2,
+                    "website": "hotstar.com",
+                    "username": "jiochutiya",
+                    "password": "iuseamazonbtw"
+                },
+                {
+                    "id": 3,
+                    "website": "facebook.com",
+                    "username": "kakashiliz",
+                    "password": "iamchutiya212"
+                },
+                {
+                    "id": 4,
+                    "website": "twitter.com",
+                    "username": "bugswriter@gmail.com",
+                    "password": "shittymarathon123"
+                }
 
-  }
+            ],
+            search: ''
+        }
+    }
+}
 </script>
 
 <style  scoped>
